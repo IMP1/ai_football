c8f32402dcbf2c6bedbadd4a739fe776ec20652f
Huw Taylor <huw_taylor@hotmail.co.uk>
2018-02-26 23:27:10 +0000

    added some fonts and a theme

--- main.lua
+++ main.lua
@@ -1,4 +1,15 @@
-local INITIAL_SCENE_CLASS = require 'scn_game'
+FONTS = {
+    system = love.graphics.getFont(),
+    game_title = love.graphics.newFont("res_PT_Sans-Web-Bold.ttf", 18),
+    game_text  = love.graphics.newFont("res_PT_Sans-Web-Bold.ttf", 12),
+}
+DEFAULT_THEME = { -- @TODO: have more than one theme?
+    void       = {0, 0, 0}, 
+    background = {224, 224, 255},
+    border     = {128, 128, 128},
+}
+
+local INITIAL_SCENE_CLASS = require 'scn_management'
 local SceneManager = require 'scn_scn_manager'
 
 -- global requires
@@ -6,12 +17,13 @@ local Vector3 = require 'lib_vec3'
 vec3 = Vector3.new
 
 function love.load()
-    local team1 = require 'cls_team'.new("Ridge Rovers", {0, 96, 255})
-    local team2 = require 'cls_team'.new("Firelight United", {255, 0, 0})
+    love.graphics.setFont(FONTS.game_title)
+    love.graphics.setBackgroundColor(DEFAULT_THEME.void)
+
+    local team1 = require 'cls_team'.new("Ridge Rovers United FC", nil, {0, 96, 255})
+    local team2 = require 'cls_team'.new("Firelight United", nil, {255, 0, 0})
     SceneManager.setScene(INITIAL_SCENE_CLASS.new(team1, team2))
 end
-
--- @TODO: put this on github.
 
 function love.update(dt)
     SceneManager.update(dt)
@@ -32,6 +44,7 @@ end
 function love.draw()
     SceneManager.draw()
     love.graphics.setColor(255, 255, 255)
+    love.graphics.setFont(FONTS.system)
     love.graphics.printf("FPS: " .. love.timer.getFPS(), 0, 0, love.graphics.getWidth(), "right")
 end
 
