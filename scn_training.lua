d72447e05786e7276c27c33297eeb3b9788e047b
Huw Taylor <huw_taylor@hotmail.co.uk>
2018-04-13 23:53:19 +0100

    reordered elelemts and furthered progress on gui

--- scn_training.lua
+++ scn_training.lua
@@ -1,10 +1,10 @@
-local gui_manager = require 'lib_gui_manager'
+local gui_manager = require 'gui_gui_manager'
 
-local List          = require 'lib_gui_scroll_list'
-local Button        = require 'lib_gui_button'
-local Text          = require 'lib_gui_text'
-local TextInput     = require 'lib_gui_text_input'
-local ColourPalette = require 'lib_gui_colour_palette'
+local List          = require 'gui_scroll_list'
+local Button        = require 'gui_button'
+local Text          = require 'gui_text'
+local TextInput     = require 'gui_text_input'
+local ColourPalette = require 'gui_colour_palette'
 
 local Scene = require 'scn_base'
 
@@ -50,14 +50,58 @@ function Training:load()
 
     local new_team = {}
     table.insert(new_team, Text.new({
-        position = {cx-w/2, 224},
+        position = {cx-w/2, 128},
         size     = {w, 32},
         text     = T"New Team",    
     }))
-    table.insert(new_team, ColourPalette.new({
-        position = {cx - w/2, 256},
-        size     = {w, w},
+    table.insert(new_team, Text.new({
+        position = {cx-w/2, 192 - 24},
+        size     = {w, 24},
+        text     = T"Team Name",    
     }))
+    table.insert(new_team, TextInput.new({
+        position    = {cx - w/2, 192},
+        size        = {w, 24},
+        placeholder = nil,
+    }))
+    table.insert(new_team, Text.new({
+        position = {cx-3*w/4, 224},
+        size     = {w, 24},
+        text     = T"Home Colour",    
+    }))
+    table.insert(new_team, Text.new({
+        position = {cx-w/4, 224},
+        size     = {w, 24},
+        text     = T"Away Colour",    
+    }))
+    local home_colour = ColourPalette.new({
+        position = {cx - w/2 + 24, 256},
+        size     = {w, w},
+        value    = {1, 1, 1}
+    })
+    local away_colour = ColourPalette.new({
+        position = {cx + 24, 256},
+        size     = {w, w},
+    })
+    home_colour.onopen = function()
+        away_colour.open    = false
+        away_colour.enabled = false
+        away_colour.opacity = 0
+    end
+    home_colour.onclose = function()
+        away_colour.opacity = 1
+        away_colour.enabled = true
+    end
+    table.insert(new_team, Button.new({
+        position = {cx - w/2, 344},
+        size     = {w, 32},
+        text     = T"Create Team",
+        onclick  = function()
+
+        end,
+    }))
+    table.insert(new_team, away_colour)
+    table.insert(new_team, home_colour)
 
     gui_manager.register('team_list', root)
     gui_manager.register('new_team', new_team)
