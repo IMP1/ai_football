bf95408ea3a5a33b8bf705ab3e3900f1f9f42f84
Huw Taylor <huw_taylor@hotmail.co.uk>
2018-04-17 23:45:33 +0100

    reworked UI and finished team creation

--- scn_training.lua
+++ scn_training.lua
@@ -1,10 +1,13 @@
 local gui_manager = require 'gui_gui_manager'
+local scn_manager = require 'scn_scn_manager'
 
 local List          = require 'gui_scroll_list'
 local Button        = require 'gui_button'
 local Text          = require 'gui_text'
 local TextInput     = require 'gui_text_input'
 local ColourPalette = require 'gui_colour_palette'
+
+local Team = require 'cls_team'
 
 local Scene = require 'scn_base'
 
@@ -20,33 +23,89 @@ function Training.new()
 end
 
 function Training:load()
-    gui_manager.reset()
-
     local root = {}
-    local team_list = {}
-    -- @TODO: load list of teams
 
     local cx, cy = love.graphics.getWidth() / 2, love.graphics.getHeight() / 2
     local w = 192
+
+    local team_list
     table.insert(root, Button.new({
-        position = {cx - w/2, 128},
-        size     = {w, 32},
+        position = {cx + 4, 288 + 48},
+        size     = {w - 4, 32},
+        text     = T"Back",
+        onclick  = function()
+            local next_scene = require('scn_singleplayer').new()
+            next_scene:load()
+            gui_manager.register('next_scene', next_scene.menu)
+            gui_manager.open('next_scene')
+            gui_manager.transitions.fade('team_list', 1, 0, 0.2)
+            gui_manager.transitions.swipe('next_scene', {-love.graphics.getWidth(), 0}, {0, 0}, 0.2, function()
+                scn_manager.popScene()
+                gui_manager.remove('next_scene')
+            end)
+        end,
+    }))
+
+    table.insert(root, Button.new({
+        position = {cx + 4, 192},
+        size     = {w - 4, 32},
         text     = T"Create New Team",
         onclick  = function()
             gui_manager.close('team_list')
             gui_manager.open('new_team')
         end,
     }))
+
     table.insert(root, Text.new({
-        position = {cx - w/2, 224},
-        size     = {w, 32},
+        position = {cx - w, 160},
+        size     = {w - 4, 32},
         text     = T"My Teams",
     }))
-    table.insert(root, List.new({
-        position = {cx - w/2, 256},
-        size     = {w, 128},
-        items    = team_list,
-    }))
+    local edit_button = Button.new({
+        position = {cx + 4, 240},
+        size     = {w - 4, 32},
+        text     = T"Edit",
+        onclick  = function()
+            local team = team_list.selected_item.data.team
+            gui_manager.close("team_list")
+            scn_manager.clearTo(require('scn_management').new(team))
+        end,
+    })
+    edit_button.enabled = false
+    table.insert(root, edit_button)
+    local test_button = Button.new({
+        position = {cx + 4, 288},
+        size     = {w - 4, 32},
+        text     = T"Test",
+        onclick  = function() 
+            -- @TODO: use value of team_list to get the team
+            --        and then go to a scn_game scene for that 
+            --        team against a house-built ai team
+        end,
+    })
+    test_button.enabled = false
+    table.insert(root, test_button)
+
+    local teams = {}
+    for _, team in ipairs(player_data.manager.teams) do
+        table.insert(teams, Text.new({
+            position = {0, #teams * 32},
+            size     = {w, 24},
+            text     = team.name,
+            data     = {team = team},
+        }))
+    end
+    team_list = List.new({
+        position = {cx - w, 192},
+        size     = {w - 4, 48 * 3 + 32},
+        items    = teams,
+        onselect = function()
+            edit_button.enabled = true
+            test_button.enabled = true
+            -- @TODO: enable the edit and test buttons
+        end,
+    })
+    table.insert(root, team_list)
 
     local new_team = {}
     table.insert(new_team, Text.new({
@@ -59,11 +118,12 @@ function Training:load()
         size     = {w, 24},
         text     = T"Team Name",    
     }))
-    table.insert(new_team, TextInput.new({
+    local name_input = TextInput.new({
         position    = {cx - w/2, 192},
         size        = {w, 24},
         placeholder = nil,
-    }))
+    })
+    table.insert(new_team, name_input)
     table.insert(new_team, Text.new({
         position = {cx-3*w/4, 224},
         size     = {w, 24},
@@ -93,11 +153,35 @@ function Training:load()
         away_colour.enabled = true
     end
     table.insert(new_team, Button.new({
-        position = {cx - w/2, 344},
-        size     = {w, 32},
+        position = {cx - w/2, 328},
+        size     = {w / 2 - 4, 32},
+        text     = T"Cancel",
+        onclick  = function()
+            gui_manager.close('new_team')
+            gui_manager.open('team_list')
+        end,
+    }))
+    table.insert(new_team, Button.new({
+        position = {cx + 4, 328},
+        size     = {w / 2 - 4, 32},
         text     = T"Create Team",
         onclick  = function()
+            local name = name_input.value
+            local manager = player_data.manager
+            local home_colour = home_colour.value
+            local away_colour = away_colour.value
+            local team = Team.new(name, manager, home_colour, away_colour)
 
+            table.insert(player_data.manager.teams, team)
+            table.insert(team_list.items, Text.new({
+                position = {0, #team_list.items * 32},
+                size     = {w, 24},
+                text     = name,
+                data     = {team = team},
+            }))
+
+            gui_manager.close('new_team')
+            gui_manager.open('team_list')
         end,
     }))
     table.insert(new_team, away_colour)
@@ -106,6 +190,8 @@ function Training:load()
     gui_manager.register('team_list', root)
     gui_manager.register('new_team', new_team)
     gui_manager.open('team_list')
+
+    self.menu = root
 end
 
 function Training:draw()
